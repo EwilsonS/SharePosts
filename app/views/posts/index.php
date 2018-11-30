9dd5678cd60093e30921bce1f2b3a04bce6db403
unknown <ewilsons@aol.com>
2018-11-29 21:47:08 -0500

    css/server

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -13,11 +13,13 @@
     <?php foreach($data['posts'] as $post) : ?>
         <div class="card card-body mb-3">
             <h4 class="card-title"><?php echo $post->title; ?></h4>
-            <div class="bg-light p-2 mb-3">
-                Written by <?php echo $post->name; ?> on <?php echo $post->postCreated; ?>
+            Written by <?php echo $post->name; ?> <br> <small> <?php echo $post->postCreated; ?></small>
+            <div class="p-2 mb-3">
             </div>
             <p class="card-text"><?php echo $post->body; ?></p>
-            <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
+            <form  action="">
+                <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark float-right">More</a>
+            </form>
         </div>
     <?php endforeach; ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
