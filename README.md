a907ee3b96febf3a50ff688423f3f01efef7a7e7
unknown <ewilsons@aol.com>
2018-11-30 09:28:55 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# shareposts
+# SharePosts
+<img src="./public/images/shaareposts.PNG" width="200px">
+<p>This is a simple social network built using my [MVC framework for PHP](https://github.com/EwilsonS/wilsonmvcphp)</p>
+<p>All forms are **validated** and **sanitized** to prevent harmful scripts</p>
+<p>Form fields use flash messages to alert the user of any errors
+<p>Only logged in users may see/add/edit posts</p>
+<br>
+
+## Here's how it works
+* Register 
+* Login (guest email: guest@gmail.com,  pass: 123456)
+* You will be redirected to the posts page
+* You may add a post or edit one of your previous posts
+* Logout to end the session
--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
   <div class="container">
         <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,11 +13,14 @@
             <li class="nav-item">
               <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://github.com/EwilsonS/shareposts" target="_blank">Github</a>
+            </li>
           </ul>
           <ul class="navbar-nav ml-auto">
             <?php if(isset($_SESSION['user_id'])) : ?>
             <li class="nav-item">
-              <span class="nav-link active" href="#">Welcom <?php echo $_SESSION['user_name']; ?></span>
+              <span class="nav-link active" href="#">Welcome <?php echo $_SESSION['user_name']; ?></span>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
