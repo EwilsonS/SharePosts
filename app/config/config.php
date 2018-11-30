723a94f86535b8cfe7c83e97e9206c65650353a1
unknown <ewilsons@aol.com>
2018-11-30 11:19:13 -0500

    m

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
     //DB Params
-    define('DB_HOST', 'y06qcehxdtkegbeb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-    define('DB_USER', 'xdi451c76lr33whn');
-    define('DB_PASS', 'ehq5appcgg7gsm3y');
-    define('DB_NAME', 'i4wapn8jlpv7h5qq');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'kalamazoo123');
+    define('DB_NAME', 'shareposts');
     
     //App Root
     define('APPROOT', dirname(dirname(__FILE__)));
 
     //URL Root
-    define('URLROOT','https://limitless-bayou-30385.herokuapp.com');
+    define('URLROOT','http://localhost/shareposts');
     
     //site name
     define('SITENAME', 'SharePosts');
